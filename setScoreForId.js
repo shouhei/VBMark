d00a99cf3701c028e7e6bba30fc5c93e5c77b3c2
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-12-08 12:00:19 +0900

    add cllf

--- setScoreForId.js
+++ setScoreForId.js
@@ -1,1 +1,8 @@
-$('.cs_table2 tr').each(function(index){var number = $(this).children().eq(1).text();for each(subject in ary){if(subject.id == number){$(this).children().eq(6).children().eq(0).val(subject.score);}}});
+$('.cs_table2 tr').each(function(index){
+    var number = $(this).children().eq(1).text();
+    for each(subject in ary){
+        if(subject.id == number){
+            $(this).children().eq(6).children().eq(0).val(subject.score);
+        }
+    }
+});
